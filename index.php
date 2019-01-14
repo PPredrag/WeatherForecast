4e6be5cf531ae045e8a61593a2b367e786d4c247
Predrag Pandza <pandzapredrag@gmail.com>
2019-01-14 22:20:07 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,10 +2,11 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
+		<!-- Global Set -->
 		<meta charset="UTF-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0,maximum-scale=1">
-    	<meta name="author" content="Predrag Pandza">
+    	        <meta name="author" content="Predrag Pandza">
 	    <link rel="icon" type="image/png" sizes="32x32" href="images/vreme.png">
 		<title>Weather</title>
 
